295d79684bff1c01461a55cf0f0f6b3c1ad2354f
LiTang <litang0617@outlook.com>
2023-05-28 09:49:42 +0800

    GxEPD Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
 #include "ePaper.h"
 
-//IO settings
-int BUSY_Pin = 32; 
-int RES_Pin = 33; 
-int DC_Pin = 5; 
-int CS_Pin = 16; 
-int SCLK_Pin = 23;
-int MOSI_Pin = 18;
+GxEPD2_3C<GxEPD2_420c_1680, GxEPD2_420c_1680::HEIGHT> display(GxEPD2_420c_1680(/*CS*/ SS_PIN, /*DC*/ DC_PIN, /*RST*/ RES_PIN, /*BUSY*/ BUSY_PIN)); // 
+
+SPIClass ePaper_SPI;
+
+void helloWorld();
 
 
-void setup() {
-   pinMode(BUSY_Pin, INPUT); 
-   pinMode(RES_Pin,  OUTPUT);  
-   pinMode(DC_Pin,   OUTPUT);    
-   pinMode(CS_Pin,   OUTPUT);    
-   //SPI
-   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); 
-   SPI.begin (SCLK_Pin,-1,MOSI_Pin,CS_Pin);  
+void setup()
+{
+  //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  ePaper_SPI.begin(CLK_PIN,-1,MOSI_PIN,SS_PIN);
+  // SPISettings(10000000, MSBFIRST, SPI_MODE0)
+  display.init(115200, true, 2, false, ePaper_SPI , SPISettings(10000000, MSBFIRST, SPI_MODE0)); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  helloWorld();
+  display.hibernate();
 }
 
-//Tips//
-/*When the electronic paper is refreshed in full screen, the picture flicker is a normal phenomenon, and the main function is to clear the display afterimage in the previous picture.
-  When the local refresh is performed, the screen does not flash.*/
-/*When you need to transplant the driver, you only need to change the corresponding IO. The BUSY pin is the input mode and the others are the output mode. */
-  
-void loop() {
-   //Full screen refresh
-    EPD_init(); //EPD init
-    PIC_display(gImage_BW,gImage_R);//EPD_picture1
-    EPD_sleep();//EPD_sleep,Sleep instruction is necessary, please do not delete!!!
-    delay(20000);    
-    
-    EPD_init(); //EPD init
-    PIC_display(gImage_BW1,gImage_R1);//EPD_picture1
-    EPD_sleep();//EPD_sleep,Sleep instruction is necessary, please do not delete!!!
-    delay(20000);   
-    
-    //Clean
-    EPD_init(); //EPD init
-    PIC_display_Clear();//EPD Clear
-    EPD_sleep();//EPD_sleep,Sleep instruction is necessary, please do not delete!!!
-    delay(20000); 
-    
+const char HelloWorld[] = "We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.";
+
+
+
+void loop() {};
+
+
+void helloWorld()
+{
+  display.setRotation(0);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
 }
